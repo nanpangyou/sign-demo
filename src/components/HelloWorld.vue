8628a81548823f0971400b9cd6940f04d1cc1838
nanpangyou <yigehd@foxmail.com>
2023-04-11 22:02:00 +0800

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { onMounted, ref } from "vue";
+import MapCanvas from "./c.ts";
+
+const cf = ref();
+onMounted(() => {
+  new MapCanvas(cf.value);
+});
+</script>
+
+<template>
+  <button>开始签名</button>
+  <button>完成</button>
+  <canvas ref="cf" class="my-canvas"></canvas>
+</template>
+
+<style scoped>
+.my-canvas {
+  height: 100vh;
+}
+</style>
